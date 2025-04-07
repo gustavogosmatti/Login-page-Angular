17d2b4d6a4348fe387f74fd413196d31133d7e5b
gustavogosmatti <gustavo.gosmatti@autogestor.net>
2025-04-07 19:25:34 -0300

    criado metodo de logout e exibindo usuario e email após login

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  user: any;
+  constructor(private authService: AuthService, private router: Router) {}
 
+  ngOnInit(){
+
+    this.getUser();
+  }
+
+  logout(): boolean {
+    try {
+      this.authService.logout().subscribe({
+        next: (response) => {
+          localStorage.removeItem('access_token');
+          console.log('Saiu com sucesso', response);
+
+          this.router.navigate(['/login']);
+        },
+        error: (error) => {
+          console.error('Erro ao sair', error);
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  getUser() {
+    this.authService.getUser().subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+      error: (error) => {
+        console.error('Erro ao buscar informações', error);
+      },
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = 'http://127.0.0.1:8000/api';
@@ -14,15 +15,22 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/login`, credentials);
   }
 
-  saveToken(token : string):void {
+  saveToken(token: string): void {
     localStorage.setItem('access_token', token);
   }
 
-  getToken() : string | null {
+  getToken(): string | null {
     return localStorage.getItem('access_token');
   }
 
-  logout(): void {
-    localStorage.removeItem('access_token');
+  logout(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/logout`, {});
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('access_token');
+  }
+  getUser()  {
+    return this.http.get(`${this.apiUrl}/user`);
   }
 }
